197fb880f30f52ce8901098a474c71bd20090eae
CarlosOrqueda <carlosorqueda@gmail.com>
2017-11-07 18:01:17 -0300

    update

--- tp2.py
+++ tp2.py
@@ -1,88 +1,71 @@
-usuarios=open("usuarios.csv","r+")
-peliculas=open("peliculas.csv","r+")
-
-def presentacion():
-          print(' #### ##            ####       #######    ##########      ########         ####                #########  ')
-          print(' #### ###         ####      #######    ##########      ########         ####                #########         ')
-          print(' #########    ####      ###                       ####               ###                       ####                        ###   ')
-          print(' ####       ## #####       #######            ####               ########         ####                       ###     ')
-          print(' ####         #######      #######           ####               ########         ####                      ###')
-          print(' ####           ######       ###                      ####               ####                    ####                      ###   ')
-          print(' ####            ######      #######          ####               ####                    ########     ####### #')
-          print(' ####               #####     #######          ####               ####                    ########     ####### #')
-
-def linea_archivo(arch,default):
-          linea=arch.readline()
-          return linea if linea else default        
-
-def leer_usuario(arch):
-          linea=linea_archivo(arch,"end,0,0,0")
-          id,nombre,fecha,peliculas=linea.strip().split(',')
-          return id,nombre,fecha,peliculas     
-          
-def grabar_usuario(arch,id,nombre,fecha,peliculas_vistas):
-          arch.readlines()
-          arch.write(id+','+nombre+','+fecha+','+peliculas_vistas+'\n')
-
-def grabar_pelicula(arch,id,nombre,director,genero,puntaje):
-          arch.readlines()
-          arch.readlines(id+','+nombre+','+director+','+genero+','+puntaje+'\n')
-          
-def alta_usuario(usuarios):
-          id=input('ID: ')
-          nombre=input('Nombre y Apellido: ')
-          fecha=input('Fecha de nacimiento ddmmaaaa: ')
-          peliculas_vistas=' '
-          grabar_usuario(usuarios,id,nombre,fecha,peliculas_vistas)
-
-def alta_pelicula(arch):
-          id=input('ID: ')
-          nombre=input('Nombre de Pelicula: ')
-          director=input('Director: ')
-          genero=input('Genero: ')
-          puntaje=input('Puntaje: ')
-          grabar_pelicula(arch,id,nombre,director,genero,puntaje)
-
-
-def baja_usuario(usuarios):
-          arch=open("bajas.csv","w")
-          nombre_a_bajar=input('Ingrese nombre: ')
-          id,nombre,fecha,peliculas = leer_usuario(usuarios)
-          while id != 'end':
-                    if nombre != nombre_a_bajar:                              
-                              arch.write(id+','+nombre+','+fecha+','+peliculas+'\n')                              
-                              id,nombre,fecha,peliculas=leer_usuario(usuarios)
-                    elif nombre == nombre_a_bajar:
-                              id,nombre,fecha,peliculas=leer_usuario(usuarios)
-                              
-          
-def menu_principal(usarios,peliculas):
-          salir=True
-          while salir == True:
-                    palabra='  Bienvenido a NetFlip  '
-                    print(palabra.center(50,"*"))
-                    print('1) Alta de Usuario')
-                    print('2) Alta de Pelicula')
-                    print('3) Baja de Usuario')
-                    print('4) Baja de Pelicula')
-                    print('5) Salir')
-                    opc = input('Opcion: ')
-
-                    if opc == '1':
-                              alta_usuario(usuarios)
-                    elif opc == '2':
-                              alta_pelicula(peliculas)
-                    elif opc == '3':
-                              baja_usuario(usuarios)
-                    elif opc == '4':
-                              baja_pelicula(peliculas)
-                    elif opc == '5':
-                              salir = False
-                              usuarios.close()
-                              peliculas.close()
-                    else:
-                              print('Opcion Incorrecta .... enter para continuar')
-                              enter=input('')
-
-presentacion()
-menu_principal(usuarios,peliculas)
+def sub_menu_peliculas():
+            palabra="Sub Menu Peliculas."
+            print(palabra.center(50,"*"))
+            print("1) Alta de Pelicula.")
+            print("2) Baja de Pelicula.")
+            print("3) Peliculas por Puntajes.")
+            print("4) Peliculas por Genero.")
+            print("5) Asignar Pelicula a Usuarios.")
+            print("6) Volver al Menu Principal.")
+            opc = input("Opcion: ")
+            if opc == '1':
+                  alta_pelicula()
+            elif opc == '2':
+                  baja_pelicula()
+            elif opc == '3':
+                  peliculas_puntaje()
+            elif opc == '4':
+                  peliculas_genero()
+            elif opc == '5':
+                  asignar_pelicula()
+            elif opc == '6':
+                  menu_principal()
+            else:
+                  enter=input("Opcion Incorrecta, enter para continuar ...")
+                  sub_menu_peliculas()
+            
+def sub_menu_user():
+            palabra="Sub Menu Usuarios."
+            print(palabra.center(50,"*"))
+            print("1) Merge.")
+            print("2) Alta de Usuario.")
+            print("3) Baja de Usuario.")
+            print("4) Listas los Usuarios.")
+            print("5) Volver al Menu Principal.")
+            opc = input("Opcion: ")
+            if opc == '1':
+                  merge()
+            elif opc == '2':
+                  alta_usuario()
+            elif opc == '3':
+                  baja_usuarios()
+            elif opc == '4':
+                  lista_usuarios()
+            elif opc == '5':
+                  menu_principal()
+            else:
+                  enter=input("Opcion Incorrecta, enter para continuar ...")
+                  sub_menu_user()
+def menu_principal():
+            salir = False
+            palabra='  Bienvenido a NetFlip  '
+            print(palabra.center(50,"*"))
+            print("1) Usurios.")
+            print("2) Peliculas.")
+            print("3) Recomendaciones.")
+            print("4) Salir.")
+            opc = input("Opcion: ")
+            if opc == '1':
+                  sub_menu_user()
+            elif opc == '2':
+                  sub_menu_peliculas()
+            elif opc == '3':
+                  sub_menu_recomendaciones()
+            elif opc == '4':
+                  salir = True
+            else:
+                  enter=input("Opcion Incorrecta, enter para continuar ...")
+                  menu_principal()
+                  
+menu_principal()
+            
